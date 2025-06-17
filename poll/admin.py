5a573b9097911fac309ed1ab7d5b8ebe980397c2
Joshik samuel.S <143866541+joshiksamuel@users.noreply.github.com>
2025-06-17 11:25:56 +0530

    Add files via upload

---
+++ poll/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+admin.site.register(models.Candidate)
+admin.site.register(models.Voter)
+admin.site.register(models.Vote)
+admin.site.register(models.Block)
+admin.site.register(models.VoterList)
+admin.site.register(models.VoterPvt)
+admin.site.register(models.VoteAuth)
+
+
+#admin.site.register(models.UserImages)
